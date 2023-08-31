0d740c91cccea2851f29d459e68c58ad4c28a1ce
mariuschev <marius.cheva@gmail.com>
2023-08-31 14:11:28 -0400

    read file

--- java/src/main.java
+++ java/src/main.java
@@ -1,5 +1,31 @@
+// first
+import java.util.Scanner;
+import java.nio.file.Paths;
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        try (Scanner scanner = new Scanner(Paths.get("DNA_data.txt"))) {
+
+            // we read the file until all lines have been read
+            while (scanner.hasNextLine()) {
+                // we read one line
+                String row = scanner.nextLine();
+                // we print the line that we read
+                System.out.println(row);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
